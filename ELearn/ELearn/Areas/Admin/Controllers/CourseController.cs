b9b8edc354d1089623c9700a564cc95f08d7961e
Konul Ibrahimova <konulsi@code.edu.az>
2023-04-12 02:56:25 +0400

    ELearn App

---
+++ ELearn/ELearn/Areas/Admin/Controllers/CourseController.cs
@@ -0,0 +1,41 @@
+﻿using ELearn.Data;
+using ELearn.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ELearn.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CourseController : Controller
+    {
+        private readonly AppDbContext _context;
+        public CourseController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async  Task<IActionResult> Index()
+        {
+            IEnumerable<Course> courses = await _context.Courses.Include(m=>m.CourseImages).Include(m=>m.Owner).Where(m => m.SoftDelete == false).OrderByDescending(m=>m.Id).ToListAsync();
+            return View(courses);
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> Detail(int? id)
+        {
+            if (id == null) return BadRequest(); 
+            Course course = await _context.Courses.Include(m => m.CourseImages).Include(m => m.Owner).Where(m => m.SoftDelete == false).FirstOrDefaultAsync(m => m.Id == id);
+
+            if (course == null) return NotFound();
+
+            return View(course);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ ELearn/ELearn/Models/Course.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace ELearn.Models
+{
+    public class Course:BaseEntity
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public int Sales { get; set; }
+        public int Price { get; set; }
+        public DateTime PublishDate { get; set; }
+        public ICollection<CourseImage> CourseImages { get; set; }
+        public int OwnerId { get; set; }
+        public Owner Owner { get; set; }
+
+
+
+
+
+    }
+}
